0ecc2f23e0468e87ae3eca8798c1de5fbdcab646
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2017-05-20 02:11:53 +0200

    feat: Add Two Numbers
    passes 1560 out of 1562 test cases. Two test cases make int overflow. To
    get rid of overflows, we need to get rid of int.

--- leetcode.go
+++ leetcode.go
@@ -1,6 +1,6 @@
 package leetcode
 
-func twoSum(nums []int, target int) []int {
+func TwoSum(nums []int, target int) []int {
 	for i, v := range nums {
 		for j, w := range nums {
 			if i == j {
@@ -12,4 +12,45 @@ func twoSum(nums []int, target int) []int {
 		}
 	}
 	return []int{-1, -1}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func val(l *ListNode) int {
+	n := 0
+	exp := 1
+	for {
+		if l == nil {
+			break
+		}
+		n += l.Val * exp
+		exp *= 10
+		l = l.Next
+	}
+	return n
+}
+
+func listNode(i int) *ListNode {
+	head := &ListNode{}
+	l := head
+	for {
+		l.Val = i % 10
+		i /= 10
+		if i == 0 {
+			break
+		}
+		l.Next = &ListNode{}
+		l = l.Next
+	}
+	return head
+}
+
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	i1 := val(l1)
+	i2 := val(l2)
+
+	return listNode(i1 + i2)
 }
--- leetcode_test.go
+++ leetcode_test.go
@@ -7,8 +7,59 @@ import (
 
 func TestTwoSum(t *testing.T) {
 	want := []int{0, 1}
-	got := twoSum([]int{2, 7, 11, 15}, 9)
+	got := TwoSum([]int{2, 7, 11, 15}, 9)
 	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("want %+v but got %+v", want, got)
+	}
+}
+
+func (l1 *ListNode) Equal(l2 *ListNode) bool {
+	for {
+		// Value does not equal
+		if (l1.Val != l2.Val) ||
+			// One list ends, the other one does not
+			(l1.Next == nil && l2.Next != nil) ||
+			(l1.Next != nil && l2.Next == nil) {
+			return false
+		}
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+	return true
+}
+
+func TestListNode(t *testing.T) {
+	got := listNode(42)
+	v := got.Val
+	if v != 2 {
+		t.Fatalf("want %v but got %v", 2, got)
+	}
+	v = got.Next.Val
+	if v != 4 {
+		t.Fatalf("want %v but got %v", 4, got)
+	}
+	if got.Next.Next != nil {
+		t.Fatalf("want %v but got %v", nil, got)
+	}
+}
+
+func TestVal(t *testing.T) {
+	want := 2017
+	got := val(listNode(want))
+	if want != got {
+		t.Fatalf("want %v but got %v", want, got)
+	}
+}
+
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+func TestAddTwoNumbers(t *testing.T) {
+	l1 := listNode(342)
+	l2 := listNode(465)
+
+	want := 807
+	got := val(AddTwoNumbers(l1, l2))
+	if want != got {
 		t.Fatalf("want %+v but got %+v", want, got)
 	}
 }
