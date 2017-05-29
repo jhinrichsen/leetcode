92555367d4e8f823fd24bb351fc24ec5339efb23
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2017-05-29 23:26:19 +0200

    feat: add LengthOfLongestSubstring()

--- leetcode.go
+++ leetcode.go
@@ -64,3 +64,22 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return listNode(i1.Add(i1, i2))
 }
+
+// LengthOfLongestSubstring returns number of bytes (not runes)
+func LengthOfLongestSubstring(s string) int {
+	max := 0
+	for j := 0; j < len(s); j++ {
+		m := make(map[byte]bool)
+		for i := j; i < len(s); i++ {
+			c := s[i]
+			if m[c] {
+				break
+			}
+			m[c] = true
+		}
+		if len(m) > max {
+			max = len(m)
+		}
+	}
+	return max
+}
--- leetcode_test.go
+++ leetcode_test.go
@@ -49,3 +49,17 @@ func TestAddTwoNumbers(t *testing.T) {
 		t.Fatalf("want %+v but got %+v", want, got)
 	}
 }
+
+func assertLengthOfLongestSubstring(t *testing.T, arg string, want int) {
+	got := LengthOfLongestSubstring(arg)
+	if want != LengthOfLongestSubstring(arg) {
+		t.Fatalf("want %v but got %v", want, got)
+	}
+}
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	assertLengthOfLongestSubstring(t, "abcabcbb", 3)
+	assertLengthOfLongestSubstring(t, "bbbbb", 1)
+	assertLengthOfLongestSubstring(t, "pwwkew", 3)
+	assertLengthOfLongestSubstring(t, "c", 1)
+}
