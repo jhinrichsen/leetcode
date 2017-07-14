bd6aa2333fc712c063bf426f6967c1b831a29a3f
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2017-07-14 11:44:37 +0200

    feat: remove output

--- palindromic_number.go
+++ palindromic_number.go
@@ -1,9 +1,5 @@
 package leetcode
 
-import (
-	"fmt"
-)
-
 // In real world, i'd rather extract a func digit(n int), but leetcode expects
 // the source code to be pasteable into a function body.
 
@@ -12,15 +8,12 @@ import (
 // Neither leetcode nor Wikipedia contain infos about negative numbers being
 // palindromic, so negative numbers never return true
 func isPalindrome(x int) bool {
-	fmt.Println("---------------------------------------------")
-	fmt.Printf("Testing %v\n", x)
 	if x < 0 {
 		return false
 	}
 
 	// Number of digits
 	digits := func(x int) int {
-		fmt.Printf("digits(%v) = ", x)
 		if x < 0 {
 			x = -x
 		}
@@ -29,7 +22,6 @@ func isPalindrome(x int) bool {
 			n++
 			x /= 10
 		}
-		fmt.Printf("%v\n", n)
 		return n
 	}
 
@@ -39,7 +31,6 @@ func isPalindrome(x int) bool {
 		for i := 0; i < n; i++ {
 			p *= x
 		}
-		fmt.Printf("pow(%v, %v) = %v\n", x, n, p)
 		return p
 	}
 
@@ -50,7 +41,6 @@ func isPalindrome(x int) bool {
 	// 1234 % 10 => 4
 	digit := func(n, d int) int {
 		y := (n / pow(10, d)) % 10
-		fmt.Printf("digit(%v, %v) = %v\n", n, d, y)
 		return y
 	}
 
@@ -58,16 +48,12 @@ func isPalindrome(x int) bool {
 	right := 0
 	for left >= right {
 		l := digit(x, left)
-		fmt.Printf("Left: %v\n", l)
 		r := digit(x, right)
-		fmt.Printf("Right: %v\n", r)
 		if l != r {
-			fmt.Printf("Left (%v) does not match Right(%v)\n", l, r)
 			return false
 		}
 		left--
 		right++
 	}
-	fmt.Printf("%v is a palindromic number\n", x)
 	return true
 }
