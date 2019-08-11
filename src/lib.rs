4d32fd6211b665573a049edb8b606fa835d54792
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2019-08-11 16:29:31 +0200

    feat: add problem #1108 (defanging an IP address)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub mod p0007_reverse_integer;
 pub mod p0008_string_to_integer_atoi;
 pub mod p0665_non_decreasing_array;
+// pub mod p0859_buddy_strings;
+pub mod p1108_defanging_an_ip_address;
---
+++ src/p1108_defanging_an_ip_address.rs
@@ -0,0 +1,28 @@
+// String as parameter is not idiomatic Rust
+pub fn defang(ip: String) -> String {
+	let mut s = String::new();
+	for c in ip.chars() {
+		if c == '.' {
+			s.push_str("[.]");
+		} else {
+			s.push(c);
+		}
+	}
+	s
+}
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn defang_1111() {
+		assert_eq!("1[.]1[.]1[.]1", super::defang("1.1.1.1".to_string()));
+	}
+
+	#[test]
+	fn defang_ip() {
+		assert_eq!(
+			"192[.]168[.]178[.]1",
+			super::defang("192.168.178.1".to_string())
+		);
+	}
+}
